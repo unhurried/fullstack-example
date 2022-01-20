1bece236033df13ed9ca1872aa937ec04187c187
unhurried <unhurried@users.noreply.github.com>
2022-01-21 06:43:50 +0900

    Change default port number to 3001.

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -18,6 +18,7 @@ async function bootstrap() {
 
   app.useGlobalPipes(new ValidationPipe({ transform: true }));
 
-  await app.listen(3000);
+  const port = process.env.PORT || 3001;
+  await app.listen(port);
 }
 bootstrap();
