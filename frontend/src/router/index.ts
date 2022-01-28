d0519e3b523f7405419da4b319130aa470c09830
unhurried <unhurried@users.noreply.github.com>
2022-01-29 07:24:39 +0900

    Change the login feature to interact with a OIDC provider.

--- frontend/src/router/index.ts
+++ frontend/src/router/index.ts
@@ -2,6 +2,7 @@ import Vue from 'vue';
 import VueRouter from 'vue-router';
 import store from '@/store';
 import Login from '../views/Login.vue';
+import Callback from '../views/Callback.vue';
 import TodoIndex from '../views/todo/Index.vue';
 import TodoList from '../views/todo/List.vue';
 import TodoCreate from '../views/todo/Create.vue';
@@ -12,6 +13,7 @@ Vue.use(VueRouter);
 const routes = [
   { path: '/', redirect: { name: 'todo/list' } },
   { path: '/login', component: Login, meta: { requiresLogin: false } },
+  { path: '/callback', component: Callback, meta: { requiresLogin: false } },
   {
     path: '/todo',
     component: TodoIndex,
---
+++ frontend/src/views/Callback.vue
@@ -0,0 +1,39 @@
+<template>
+  <div>loading ...</div>
+</template>
+
+<script lang="ts">
+import { RedirectRequestHandler, LocalStorageBackend, DefaultCrypto, AuthorizationNotifier, AuthorizationServiceConfiguration, FetchRequestor, BaseTokenRequestHandler, TokenRequest, GRANT_TYPE_AUTHORIZATION_CODE, BasicQueryStringUtils, LocationLike, StringMap } from '@openid/appauth';
+import { Component, Vue } from 'vue-property-decorator';
+import NoHashQueryStringUtils from '../login/no-hash-query';
+
+@Component({ name: 'Callback' })
+export default class Callback extends Vue {
+  private async beforeCreate() {
+    const authorizationHandler = new RedirectRequestHandler(
+      new LocalStorageBackend(),
+      new NoHashQueryStringUtils(),
+      window.location,
+      new DefaultCrypto(),
+    );
+
+    const notifier = new AuthorizationNotifier();
+    notifier.setAuthorizationListener(async (request, response, error) => {
+      const config = await AuthorizationServiceConfiguration.fetchFromIssuer('http://localhost:3002', new FetchRequestor());
+      const tokenHandler = new BaseTokenRequestHandler(new FetchRequestor());
+      const req = new TokenRequest({
+        client_id: 'client_id_for_vue',
+        redirect_uri: 'http://localhost:8080/callback',
+        grant_type: GRANT_TYPE_AUTHORIZATION_CODE,
+        code: response?.code,
+        extras: { code_verifier: request.internal!.code_verifier },
+      });
+      const res = await tokenHandler.performTokenRequest(config, req);
+      this.$store.commit('auth/update', { username: res.idToken?.sub, accessToken: res.accessToken });
+      this.$router.push({ name: 'todo/list' });
+    });
+    authorizationHandler.setAuthorizationNotifier(notifier);
+    authorizationHandler.completeAuthorizationRequestIfPossible();
+  }
+}
+</script>
